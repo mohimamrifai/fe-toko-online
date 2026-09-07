69fcc3105668705167a707cf613eb954598179d6
mohimamrifai <imamrifaibisnis@gmail.com>
2026-09-07 14:10:09 +0700

    create promo slider component, add image promo banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
+import { PromoSlider } from "@/components/commons/promo-slider";
 import { Header } from "@/components/layout/header";
 
 export default function Home() {
   return (
     <main>
       <Header />
+      <PromoSlider />
+      {/* <CategoryShortcuts /> */}
+      {/* <FlashSale /> */}
+      {/* <ProductGrid title="Terlaris" /> */}
+      {/* <PromoBanner /> */}
+      {/* <ProductGrid title="Baru Masuk" /> */}
+      {/* <ProductGrid title="Rekomendasi untukmu" /> */}
+      {/* <Footer /> */}
     </main>
   );
 }
